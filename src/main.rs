3644646a4cc8f800936c432ae945d19f76f94177
wolflieu201105 <201105phamgiahuy@gmail.com>
2025-12-19 17:32:35 -0600

    added exit

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@ use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write, stdin};
 
+fn echo(inputs: Vec<&str>) {
+    for arg in inputs.iter().skip(1) {
+        print!("{} ", arg);
+    }
+    println!();
+    io::stdout().flush().unwrap();
+}
+
 fn main() {
     let mut commands: HashMap<&str, i16> = HashMap::new();
     commands.insert("exit", 0);
+    commands.insert("echo", 1);
 
     let mut input_command = String::new();
 
@@ -18,16 +27,23 @@ fn main() {
             .read_line(&mut input_command)
             .expect("Failed to read line");
 
-        let input: &str = input_command.trim();
+        let inputs: Vec<&str> = input_command.trim().split(' ').collect();
 
-        if (!commands.contains_key(&input)) {
-            print!("{}: command not found\n", input);
+        if inputs.is_empty() {
+            continue;
+        }
+
+        let command = inputs[0];
+
+        if (!commands.contains_key(command)) {
+            print!("{}: command not found\n", command);
             io::stdout().flush().unwrap();
             continue;
         }
 
-        match commands.get(input) {
+        match commands.get(command) {
             Some(&0) => break,
+            Some(&1) => echo(inputs),
             Some(&_) => (),
             None => (),
         }
